e37f1fcb218637f8d9265d434692752f36258d7e
unfuz3 <mr.mendozamen@gmail.com>
2024-03-17 22:42:17 +0100

    Functional propositions

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from itertools import product
+from copy import deepcopy
 
-LETTERS = ["P","Q","R"]
+# Symbols: ¬ V ∧ → ⇔
+# ------------------------
+# TODO: Translator from formal logic language to lists
+# ------------------------
+
+LETTERS = ["P","Q"]
 
 class Proposition():
     def __init__(self, type:str=None, op1=None, op2=None) -> None:
@@ -11,34 +17,52 @@ class Proposition():
         if self.type == "atom":
             self.display = self.op1
         elif self.type == "not":
-            self.display = f"¬({self.op1})"
+            self.display = f"(¬{self.op1.display})"
         elif self.type == "and":
             self.display = f"({self.op1.display} ∧ {self.op2.display})"
         elif self.type == "or":
             self.display = f"({self.op1.display} V {self.op2.display})"
+        elif self.type == "implies":
+            self.display = f"({self.op1.display} → {self.op2.display})"
     
     def __repr__(self) -> str:
         return self.display
 
     # evaluate returns the evaluation of a single interpretation
+    # In: interp Out: bool
     def evaluate(self, interp) -> bool:
         if interp == None:
-            raise TypeError("Predicate's interpretation is empty")
+            raise ValueError("Predicate's interpretation is empty")
 
         if self.type == None:
             raise ValueError("Predicate's type is None")
 
         if self.type == "atom":
-            return interp[self.op1]
+            res = interp[self.op1]
         elif self.type == "and":
-            return self.op1.evaluate(interp) and self.op2.evaluate(interp)
+            res = self.op1.evaluate(interp) and self.op2.evaluate(interp)
         elif self.type == "or":
-            return self.op1.evaluate(interp) or self.op2.evaluate(interp)
+            res = self.op1.evaluate(interp) or self.op2.evaluate(interp)
         elif self.type == "not":
-            return not self.op1.evaluate(interp)
+            res = not self.op1.evaluate(interp)
+        elif self.type == "implies":
+            res = (not self.op1.evaluate(interp)) or self.op2.evaluate(interp)
         
+        return int(res)
+    
+    # In: interps Out: bevals
+    def evaluate_all(self,interps:list[dict]):
+        lis = []
 
-def generate_list_of_interps(letters:list) -> list[dict]:
+        for interp in interps:
+            beval = deepcopy(interp)
+            beval["result"] = self.evaluate(beval)
+            lis.append(beval)
+        
+        return lis
+        
+# In: letters, Out: Interps
+def generate_interps(letters:list) -> list[dict]:
     vals = product([0,1], repeat=len(letters))
     lis = []
 
@@ -48,40 +72,48 @@ def generate_list_of_interps(letters:list) -> list[dict]:
     lis.reverse()
 
     return lis
-"""
-This function is not implemented as it should be. Instead of generating interpretations, it
-should be given a list with the desired interpretations to display.
 
-def show_table(prop:Proposition, letters:list[str]):
-    abcs = generate_abc(letters)
+
+def show_table(prop:Proposition, evals:list[dict]) -> None:
+    letters = list(evals[0].keys())
+    letters.remove("result")
 
     for l in letters:
         print(f" {l} |", end="")
     print(f" {prop}")
 
-    for abc in abcs:
+    for eval in evals:
         for l in letters:
-            print(f" {abc[l]} |", end="")
-        print(f" {prop.evaluate(abc)}")
-"""
+            print(f" {eval[l]} |", end="")
+        print(f" {eval['result']}")
 
-# TODO: Proposition generator, instead of the shit that's in main()
-# [[["P","atom"],or,["Q","atom"]],"and",[["R","atom"],"not"]] = ((P V Q) ∧ ¬(R))
 
 def build_proposition(proptree:list) -> Proposition:
     if proptree[1] == "atom":
         return Proposition("atom",proptree[0])
-    elif proptree[1] == "not":
-        return Proposition("not",build_proposition(proptree[0]))
-    elif proptree[1] == "and":
-        return Proposition("and",build_proposition(proptree[0]),build_proposition(proptree[2]))
-    elif proptree[1] == "or":
-        return Proposition("or",build_proposition(proptree[0]),build_proposition(proptree[2]))
+    elif proptree[1] in ["not"]:
+        return Proposition(proptree[1],build_proposition(proptree[0]))
+    elif proptree[1] in ["and","or","implies"]:
+        return Proposition(proptree[1],build_proposition(proptree[0]),build_proposition(proptree[2]))
+
+def are_equal(prop1:Proposition, prop2:Proposition, letters:list[str]) -> bool:
+    interps = generate_interps(letters)
+    return prop1.evaluate_all(interps) == prop2.evaluate_all(interps)
 
 def main():
-    proptree = [[["P","atom"],"or",["Q","atom"]],"and",[["R","atom"],"not"]]
+    proptree = [["P","atom"],"implies",["Q","atom"]]
+    proptree2 = [[["P","atom"],"not"],"and",["Q","atom"]]
     prop = build_proposition(proptree)
-    print(prop)
+    prop2 = build_proposition(proptree2)
+    interps = generate_interps(LETTERS)
+    evals = prop.evaluate_all(interps)
+    evals2 = prop2.evaluate_all(interps)
+    show_table(prop, evals)
+    print()
+    show_table(prop2,evals2)
+    print()
+    print(are_equal(prop,prop2,LETTERS))
+
 
 if __name__=="__main__":
     main()
